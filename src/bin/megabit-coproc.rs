2f5b2e0949dfe328e3a45e1ad5a70a103cda80fd
Shane Snover <ssnover95@gmail.com>
2024-02-06 12:16:18 -0700

    USB echo working

--- src/bin/megabit-coproc.rs
+++ src/bin/megabit-coproc.rs
@@ -6,19 +6,78 @@ use embassy_nrf::{
     bind_interrupts,
     gpio::{Level, Output, OutputDrive},
     peripherals, spim,
+    usb::{self, vbus_detect::HardwareVbusDetect},
 };
 use embassy_time::Timer;
+use embassy_usb::{
+    class::cdc_acm::{self, CdcAcmClass},
+    driver::EndpointError,
+};
 use megabit_coproc_embassy::DotMatrix;
 use panic_probe as _;
+use static_cell::StaticCell;
 
 bind_interrupts!(struct Irqs {
+    POWER_CLOCK => usb::vbus_detect::InterruptHandler;
     SPIM3 => spim::InterruptHandler<peripherals::SPI3>;
+    USBD => usb::InterruptHandler<peripherals::USBD>;
 });
 
+type UsbDriver = usb::Driver<'static, peripherals::USBD, HardwareVbusDetect>;
+
+#[embassy_executor::task]
+async fn usb_driver_task(mut device: embassy_usb::UsbDevice<'static, UsbDriver>) {
+    device.run().await;
+}
+
+#[embassy_executor::task]
+async fn ping_response_task(mut class: CdcAcmClass<'static, UsbDriver>) {
+    loop {
+        class.wait_connection().await;
+        let _ = handle_ping(&mut class).await;
+    }
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: embassy_executor::Spawner) {
+async fn main(spawner: embassy_executor::Spawner) {
     let nrf_peripherals = embassy_nrf::init(Default::default());
     let mut led = Output::new(nrf_peripherals.P0_06, Level::Low, OutputDrive::Standard);
+
+    let usb_driver = usb::Driver::new(nrf_peripherals.USBD, Irqs, HardwareVbusDetect::new(Irqs));
+    let mut config = embassy_usb::Config::new(0x16c0, 0x27de);
+    config.manufacturer = Some("Snostorm Labs");
+    config.product = Some("Megabit coproc");
+    config.serial_number = Some("0123456789ABCDEF");
+    config.max_power = 125;
+    config.max_packet_size_0 = 64;
+    config.device_class = 0xEF;
+    config.device_sub_class = 0x02;
+    config.device_protocol = 0x01;
+    config.composite_with_iads = true;
+
+    static STATE: StaticCell<cdc_acm::State> = StaticCell::new();
+    let state = STATE.init(cdc_acm::State::new());
+
+    static DEVICE_DESC: StaticCell<[u8; 256]> = StaticCell::new();
+    static CONFIG_DESC: StaticCell<[u8; 256]> = StaticCell::new();
+    static BOS_DESC: StaticCell<[u8; 256]> = StaticCell::new();
+    static MSOS_DESC: StaticCell<[u8; 128]> = StaticCell::new();
+    static CONTROL_BUF: StaticCell<[u8; 128]> = StaticCell::new();
+    let mut builder = embassy_usb::Builder::new(
+        usb_driver,
+        config,
+        &mut DEVICE_DESC.init([0; 256])[..],
+        &mut CONFIG_DESC.init([0; 256])[..],
+        &mut BOS_DESC.init([0; 256])[..],
+        &mut MSOS_DESC.init([0; 128])[..],
+        &mut CONTROL_BUF.init([0; 128])[..],
+    );
+    let class = CdcAcmClass::new(&mut builder, state, 64);
+    let usb = builder.build();
+
+    spawner.spawn(usb_driver_task(usb)).unwrap();
+    spawner.spawn(ping_response_task(class)).unwrap();
+
     let mut config = spim::Config::default();
     config.frequency = spim::Frequency::M4;
     config.mode = spim::MODE_0;
@@ -49,5 +108,24 @@ async fn main(_spawner: embassy_executor::Spawner) {
         Timer::after_millis(300).await;
         led.set_low();
         Timer::after_millis(300).await;
+    }
+}
+
+struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+async fn handle_ping(class: &mut CdcAcmClass<'static, UsbDriver>) -> Result<(), Disconnected> {
+    let mut buf = [0; 64];
+    loop {
+        let bytes_read = class.read_packet(&mut buf).await?;
+        class.write_packet(&buf[..bytes_read]).await?;
     }
 }
